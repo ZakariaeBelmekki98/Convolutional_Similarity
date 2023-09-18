dc9b8095260281e05f3858c6752cf9466550bcd4
zak <zakariae.belmekki@outlook.com>
2023-09-18 14:24:32 +0100

    added Utils

---
+++ Utils.py
@@ -0,0 +1,13 @@
+import torch
+
+def model_accuracy(model, dataloader, device="cpu"):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return correct/total*100
