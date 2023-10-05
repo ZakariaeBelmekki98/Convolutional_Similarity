062be10c5c214f523e598c5582a520d37901038b
zaktron <zakariae.belmekki@cranfield.ac.uk>
2023-10-05 11:49:46 +0100

    ImageNet updated

--- ImageNet.py
+++ ImageNet.py
@@ -11,11 +11,40 @@ import torchvision.transforms as transforms
 from torchvision.models import resnet50
 
 BATCH_SIZE = 128
+EPOCHS=20
 DEVICE="cuda"
 DIR = "~/imagenet/ILSVRC/Data/CLS-LOC/"
 TRAIN_DIR = os.path.join(DIR, 'train')
 VAL_DIR = os.path.join(DIR, 'val')
 
+###
+# Useful functions
+###
+
+def accuracy(output, target, topk=(1,)):
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+def validate(loader, model):
+    with torch.no_grad():
+        for i, (images, target) in enumerate(loader):
+            images = images.to(DEVICE, non_blocking=True)
+            target = target.to(DEVICE, non_blocking=True)
+
+            output = model(images)
+            loss = criterion(output, target)
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            print(acc1, acc5)
 
 
 if __name__ == "__main__":
@@ -30,5 +59,23 @@ if __name__ == "__main__":
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=8, pin_memory=True)
     
     model = resnet50().to(DEVICE)
-    print(model)
+    
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+    criterion = nn.CrossEntropyLoss().to(DEVICE)
+
+    for epoch in range(EPOCHS):
+        for i, (images, target) in enumerate(train_loader):
+            images = images.to(DEVICE, non_blocking=True)
+            target = target.to(DEVICE, non_blocking=True)
+
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+
+            if i % 50 == 0:
+                print("[{}/{}] Loss: {:.3f}".format(epoch, EPOCHS, loss.item()))
+
+        validate(val_loader, model)
 
